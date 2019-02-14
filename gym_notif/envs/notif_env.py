6a8df81eb77a4c66cbc7dea76f42d3cc93d73b19
Rowan Sutton <sutton.r11@gmail.com>
2019-02-14 15:46:01 +0000

    Updated to read from CSV file and store in Notification objects.

--- gym_notif/envs/notif_env.py
+++ gym_notif/envs/notif_env.py
@@ -1,18 +1,57 @@
 import gym
+import csv
 from gym import error, spaces, utils
 from gym.utils import seeding
+from gym_notif.envs.mobile_notification import Notification
 
 
 # NEED TO INSTALL ENVIRONMENT WITH "pip install -e ." IN PROJECT PARENT DIR
 class NotifEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
+    CSV_FILE = "notif_user_2.csv"
+
     def __init__(self):
-        self.state = "GROUND"  # State of notifications?
+        # ----- Initialize environment variables -----
+        self.state = []  # State of notifications?
         self.reward = 0  # Initial reward of zero
         self.done = False  # We are not finished at the start
         self.info = 0  # Extra diagnostic info (can also be useful for learning)
         self.counter = 0  # A counter to use so that there's a limited number of iterations
+
+        # ----- Load in CSV file -----
+        # Takes columns in order as they are presented in csv
+        fieldnames = ("index", "action", "appPackage", "category", "postedTimeOfDay")
+
+        with open(self.CSV_FILE) as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames)
+            for row in reader:
+                self.state.append(Notification(row["appPackage"], row["category"], row["postedTimeOfDay"]))
+
+        self.state.pop(0)  # Remove Notif object containing table headers
+
+        print("OOOOOOOOOOOO")
+        print(self.state[0])
+        print("OOOOOOOOOOOO")
+
+        # Find all possible values for packages, categories and ToD
+        package_states = []
+        category_states = []
+        time_of_day_states = []
+        for item in self.state:
+            if item.appPackage not in package_states:
+                package_states.append(item.appPackage)
+            if item.category not in category_states:
+                    category_states.append(item.category)
+            if item.postedTimeOfDay not in time_of_day_states:
+                    time_of_day_states.append(item.postedTimeOfDay)
+
+        print(package_states)
+        print(category_states)
+        print(time_of_day_states)
+
+        total_num_states = len(package_states) * len(category_states) * len(time_of_day_states)
+        print("Total number of Notification states: " + str(total_num_states))
 
     def step(self, action):
         # "Accepts an action and returns a tuple (observation, reward, done, info)."
@@ -21,18 +60,15 @@ class NotifEnv(gym.Env):
         if self.done:
             return [self.state, self.reward, self.done, self.info]
         else:
+            self.reward = 0
             if action == "JUMP":
                 if self.state == "GROUND":
                     self.reward = 1
                     self.state = "AIR"
-                if self.state == "AIR":
-                    self.reward = 0
             elif action == "DUCK":
                 if self.state == "AIR":
                     self.reward = 1
                     self.state = "GROUND"
-                if self.state == "GROUND":
-                    self.reward = 0
             self.counter += 1
             if self.counter > 9:
                 self.done = True
@@ -49,4 +85,4 @@ class NotifEnv(gym.Env):
 
     def render(self, mode='human', close=False):
         if mode == 'human':
-            print("Currently in: " + self.state + "with reward: " + self.reward)
+            print("Currently in: with reward: " + str(self.reward))
