2a0f639f8bc2bc77bc16df531c75232a67dc5f03
Rowan Sutton <sutton.r11@gmail.com>
2019-02-16 15:12:28 +0000

    Improved CSV loading with Pandas implementation

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name='gym_notif',
       packages=find_packages(),
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym', 'pandas']  # And any other dependencies foo needs
 )
