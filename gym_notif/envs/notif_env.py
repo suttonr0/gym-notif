7173535375d9ddfbb8f3ccf9d6779c0f162bb2ea
Rowan Sutton <sutton.r11@gmail.com>
2019-02-17 15:27:03 +0000

    Implemented step function

--- gym_notif/envs/notif_env.py
+++ gym_notif/envs/notif_env.py
@@ -8,18 +8,18 @@ from gym_notif.envs.mobile_notification import MobileNotification
 # NEED TO INSTALL ENVIRONMENT WITH "pip install -e ." IN PROJECT PARENT DIR
 class NotifEnv(gym.Env):
     metadata = {'render.modes': ['human']}
-
     CSV_FILE = "notif_user_2.csv"
 
     def __init__(self):
         # ----- Initialize environment variables -----
-        self.state = []  # State of notifications?
+        self.state = []  # Current notification to make a decision for
         self.reward = 0  # Initial reward of zero
         self.done = False  # We are not finished at the start
         self.info = 0  # Extra diagnostic info (can also be useful for learning)
         self.counter = 0  # A counter to use so that there's a limited number of iterations
         self.notification_list = []  # A list of Notification objects from the CSV file
         self.total_states = 0  # The total number of different notification states
+        self.previous_action = []  # The last action taken as input to the environment
 
         # ----- Load in CSV file -----
         # Obtain action, appPackage, category and postedTimeOfDay
@@ -29,10 +29,7 @@ class NotifEnv(gym.Env):
         for n in range(0, len(notif_action)):
             self.notification_list.append(MobileNotification(df.action[n], df.appPackage[n], df.category[n],
                                                              df.postedTimeOfDay[n]))
-            print(self.notification_list[n])
-
         self.state = self.notification_list[0]  # Initialize to first value
-        print(self.notification_list[2].appPackage)
 
         # Find all possible values for packages, categories and ToD
         package_states = []
@@ -46,47 +43,52 @@ class NotifEnv(gym.Env):
             if item.postedTimeOfDay not in time_of_day_states:
                 time_of_day_states.append(item.postedTimeOfDay)
 
-        print("Total unique Package States: ")
+        print("ENV: Total unique Package States: ")
         print(package_states)
-        print("Total unique Category States: ")
+        print("ENV: Total unique Category States: ")
         print(category_states)
-        print("Total unique ToD States: ")
+        print("ENV: Total unique ToD States: ")
         print(time_of_day_states)
 
         self.total_states = len(package_states) * len(category_states) * len(time_of_day_states)
-        print("Total number of Notification states: " + str(self.total_states))
+        print("ENV: Total number of Notification states: " + str(self.total_states))
 
     def step(self, action):
         # "Accepts an action and returns a tuple (observation, reward, done, info)."
         # Should take in the action, change the state dependent on that action, calculate
         # the reward and return the [self.state, self.reward, self.done, self.info]
         if type(action) != bool:
-            print("ERROR: Incorrect type for 'action'. Requires type bool.")
+            print("ENV: ERROR: Incorrect type for 'action'. Requires type bool.")
             return -1
         if self.done:
+            # Finished, just return data
             return [self.state, self.reward, self.done, self.info]
         else:
             self.reward = 0
             if action == self.notification_list[self.counter].action:
+                # If the action taken by the user matches the action provided by the RL system
                 self.reward = 1
-                self.state = "AIR"  # Can change state here if result was dependent on the reward
             elif action:
-                print(self.notification_list[self.counter])
-                self.state = "GROUND"
+                self.reward = -1
             self.counter += 1
+            # Update state
+            self.state = self.notification_list[self.counter]
             if self.counter > 9:
                 self.done = True
+            self.previous_action = action
             self.render()
-
         return [self.state, self.reward, self.done, self.info]
 
     def reset(self):
-        self.state = "GROUND"  # State of notifications?
+        self.state = self.notification_list[0]  # Initialize to first value
         self.reward = 0  # Initial reward of zero
         self.done = False  # We are not finished at the start
-        self.info = 0
+        self.info = 0  # Extra diagnostic info (can also be useful for learning)
+        self.counter = 0  # A counter to use so that there's a limited number of iterations
+
         return self.state
 
     def render(self, mode='human', close=False):
         if mode == 'human':
-            print("Currently in: with reward: " + str(self.reward))
+            print("ENV: Current notification: {}\n\t with calculated reward: {}\n\t based on input action: {}".format(
+                self.state, self.reward, self.previous_action))
